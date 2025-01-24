b0b4b98a761e2521a7113a0d42d336ace23eecaa
chris-at-lemon <cherrmann@lemon-digital.com>
2025-01-24 21:32:01 +0100

    base layout

---
+++ src/app/fonts.ts
@@ -0,0 +1,12 @@
+import { Cardo, Nunito } from "next/font/google";
+
+export const cardo = Cardo({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "700",
+});
+
+export const nunito = Nunito({
+  subsets: ["latin"],
+  display: "swap",
+});
