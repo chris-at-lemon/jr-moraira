86e7be4a23d5e2a7c99798d513045e975bc1e049
chris-at-lemon <cherrmann@lemon-digital.com>
2025-02-01 14:41:20 +0100

    accordion fix

--- src/components/accordeon/line.tsx
+++ src/components/accordeon/line.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export const Line = () => {
-  return <div className="my-4 h-0.5 w-full bg-neutral-200 md:my-6"></div>;
+  return (
+    <div className="mb-5 mt-4 h-[1px] w-full bg-neutral-300 md:mb-6 md:mt-5"></div>
+  );
 };
