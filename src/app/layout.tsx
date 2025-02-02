d97058a40a50750e5d88ca3ca3f6b4f0677e76a9
chris-at-lemon <cherrmann@lemon-digital.com>
2025-02-02 11:24:18 +0100

    add meta tags and open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,23 @@ config.autoAddCss = false;
 
 export const metadata: Metadata = {
   title: "Reykja und John",
-  description: "Hochzeit in Moraira",
+  description: "Reykja und John's Hochzeit in Moraira",
+  openGraph: {
+    title: "Reykja und John",
+    description: "Reykja und John's Hochzeit in Moraira",
+    url: "https://www.jr-moraira.com",
+    siteName: "Reykja und John",
+    images: [
+      {
+        url: "https://ik.imagekit.io/vcqe1lhbs/jr-moraira/j&r_oxLU19r4q.png?updatedAt=1738491271132",
+        width: 512,
+        height: 512,
+      },
+    ],
+    locale: "de_DE",
+    alternateLocale: ["de_DE", "en_UK"],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
